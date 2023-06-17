cf3affb215928f98220c351d6e5f84345567e10e
Syava Dee <syavadee@gmail.com>
2023-06-17 14:26:48 +0000

    migrate to http

--- ctrl/middleware.go
+++ ctrl/middleware.go
@@ -1,88 +1,11 @@
 package ctrl
 
 import (
-	"bufio"
-	"fmt"
-	. "github.com/mickael-kerjean/webpty/common"
-	"github.com/patrickmn/go-cache"
-	"golang.org/x/crypto/ssh"
-	"net"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func Middleware(fn func(res http.ResponseWriter, req *http.Request)) func(res http.ResponseWriter, req *http.Request) {
-	tmpCache := cache.New(5*time.Minute, 10*time.Minute)
 	return func(res http.ResponseWriter, req *http.Request) {
-		startTime := time.Now()
-		username, password, ok := req.BasicAuth()
-		defer func() {
-			Log.Info(
-				"HTTP %.1fms %s %s",
-				float32(time.Now().Sub(startTime).Microseconds())/1000,
-				func() string {
-					if ok == false {
-						return "anonymous"
-					}
-					return username
-				}(),
-				req.URL.Path,
-			)
-		}()
-		if ok == false {
-			Log.Error("basic authentication error")
-			res.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-			ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
-			return
-		}
-		if _, found := tmpCache.Get(username + ":" + password); found == false {
-			var err error = nil
-			// if username != "test" || password != "test" {
-			// 	ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
-			// 	err = ErrNotAuthorized
-			// 	return
-			// }
-			sshPort := func() int {
-				p := 22
-				file, err := os.OpenFile("/etc/ssh/sshd_config", os.O_RDONLY, os.ModePerm)
-				if err != nil {
-					return p
-				}
-				scanner := bufio.NewScanner(file)
-				for scanner.Scan() {
-					line := strings.TrimSpace(scanner.Text())
-					prefix := "Port"
-					if strings.HasPrefix(line, prefix) {
-						n, err := strconv.Atoi(strings.TrimSpace(strings.TrimPrefix(line, prefix)))
-						if err != nil {
-							Log.Error("sshd cannot parse port from /etc/ssh/sshd_config")
-						}
-						p = n
-						break
-					}
-				}
-				file.Close()
-				return p
-			}()
-			client, err := ssh.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", sshPort), &ssh.ClientConfig{
-				User: username,
-				Auth: []ssh.AuthMethod{ssh.Password(password)},
-				HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-					return nil
-				},
-			})
-			if err != nil {
-				Log.Error("sshd authentication error: %s", err.Error())
-				res.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-				ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
-				return
-			}
-			client.Close()
-			tmpCache.Set(username+":"+password, nil, cache.DefaultExpiration)
-		}
 		fn(res, req)
 	}
 }
--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"crypto/tls"
 	_ "embed"
 	"fmt"
-	. "github.com/mickael-kerjean/webpty/common"
-	"github.com/mickael-kerjean/webpty/common/ssl"
-	"github.com/mickael-kerjean/webpty/ctrl"
 	"net/http"
 	"os"
 	"strconv"
+
+	. "github.com/mickael-kerjean/webpty/common"
+	"github.com/mickael-kerjean/webpty/ctrl"
 )
 
 var port int = 3456
+var addr string = "localhost"
 
 func init() {
 	if pStr := os.Getenv("PORT"); pStr != "" {
 		if pInt, err := strconv.Atoi(pStr); err == nil {
 			port = pInt
 		}
+	}
+	if pStr := os.Getenv("ADDR"); pStr != "" {
+		addr = pStr
 	}
 }
 
@@ -40,40 +43,15 @@ func main() {
 
     Web Interface:
 `
-	for _, url := range GetAddress() {
-		msg += fmt.Sprintf("    - https://%s:%d\n", url, port)
-	}
+	msg += fmt.Sprintf("    - http://%s:%d\n", addr, port)
 	Log.Stdout(msg + "\nLOGS:")
-	TLSCert, _, err := ssl.GenerateSelfSigned()
-	if err != nil {
-		Log.Error("ssl.GenerateSelfSigned %s", err.Error())
-		return
-	}
 	Log.Info("WebPty is ready to go")
 
 	if err := (&http.Server{
-		Addr:         fmt.Sprintf(":%d", port),
-		Handler:      mux,
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			},
-			PreferServerCipherSuites: true,
-			CurvePreferences: []tls.CurveID{
-				tls.CurveP256,
-				tls.X25519,
-			},
-			Certificates: []tls.Certificate{TLSCert},
-		},
+		Addr:     fmt.Sprintf("%s:%d", addr, port),
+		Handler:  mux,
 		ErrorLog: NewNilLogger(),
-	}).ListenAndServeTLS("", ""); err != nil {
+	}).ListenAndServe(); err != nil {
 		Log.Error("[https]: listen_serve %v", err)
 	}
 }
